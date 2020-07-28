1d8d9ea7905bfa6851ed6d2ad3a77db6361d646d
Cameron Kirby <cameronevankirby@gmail.com>
2020-07-28 15:59:36 -0500

    Changed Login response

--- auth/auth-router.js
+++ auth/auth-router.js
@@ -44,7 +44,7 @@ router.post('/login', (req, res) => {
                 if (user && bcryptjs.compareSync(password, user.password)) {
                     const token = makeJwt(user);
 
-                    res.status(200).json({ message: "Welcome to our API", token });
+                    res.status(200).json({ data: user, token });
                 } else {
                     res.status(401).json({ message: "Invalid credentials" });
                 }
